530f4993c0c43594aa63bef779c835be835baba9
Matthew Poulter <github@poulter.co.za>
2018-03-15 18:44:17 +0200

    Attempt 2.0

---
+++ src/ChatClient.java
@@ -0,0 +1,10 @@
+/*
+ * Student No.: PLTMAT001, MDLKHA012, RTTCHA002
+ * Assignment: 1
+ * Course: CSC3002F
+ * Date: 15 3 2018
+ * Copyright (c) 2018. PLTMAT001, MDLKHA012, RTTCHA002
+ */
+
+public class ChatClient {
+}

0e66c0e05cf505e936239003c898985df790c42f
Matthew Poulter <github@poulter.co.za>
2018-03-16 09:28:05 +0200

    Small change

--- src/ChatServer.java
+++ src/ChatServer.java
@@ -2,9 +2,10 @@
  * Student No.: PLTMAT001, MDLKHA012, RTTCHA002
  * Assignment: 1
  * Course: CSC3002F
- * Date: 15 3 2018
+ * Date: 16 3 2018
  * Copyright (c) 2018. PLTMAT001, MDLKHA012, RTTCHA002
  */
 
 public class ChatServer {
+    //TODO: Rewrite server
 }
